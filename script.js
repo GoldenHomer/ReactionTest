91b01a82f551263280f27512ae6eb3261a276e67
Jose Contreras <jocontre@joselcontreras.com>
2014-12-03 23:29:00 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function getRandomColor() {
   var letters = '0123456789ABCDEF'.split('');
   var color = '#';
-  for (var i = 0; i < 6; i++ ) {
-      color += letters[Math.round(Math.random() * 15)];
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.round(Math.random() * 15)];
   }
     return color;
 }
@@ -13,10 +13,10 @@ var reactionTime;
             
   function makeBox() {
     var time=Math.random();
-    time=*5000;
+    time *= 5000;
 
     setTimeout(function() {
-      if (Math.random()>0.5) {
+      if (Math.random() > 0.5) {
         document.getElementById("box").style.borderRadius="100px";￼￼
       } 
       else {
@@ -24,10 +24,10 @@ var reactionTime;
       }
           
       var top=Math.random();
-      top=*300;
+      top*=300;
           
       var left=Math.random();
-      left=*500;
+      left*=500;
                               
       document.getElementById("box").style.top=top+"px";
       document.getElementById("box").style.left=left+"px";
@@ -38,8 +38,8 @@ var reactionTime;
 ￼￼}
 document.getElementById("box").onclick=function() {
   clickedTime=Date.now();
-  reactionTime=(clickedTime-createdTime)/1000;
-  document.getElementById("time").innerHTML=reactionTime;
+  reactionTime = (clickedTime-createdTime)/1000;
+  document.getElementById("time").innerHTML = reactionTime;
   this.style.display="none";
   makeBox(); 
 }
